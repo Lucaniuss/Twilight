89d7dbf0bef205931f81df490e3e90c1ac213efd
Lucanius69 <lucaisdik7@gmail.com>
2022-05-22 19:51:42 +0200

    Create BoardLayout.java

---
+++ src/main/java/me/lucanius/prac/layout/BoardLayout.java
@@ -0,0 +1,38 @@
+package me.lucanius.prac.layout;
+
+import lombok.RequiredArgsConstructor;
+import me.lucanius.prac.Twilight;
+import me.lucanius.prac.tools.CC;
+import me.lucanius.prac.tools.board.BoardAdapter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Lucanius
+ * @since May 22, 2022
+ */
+@RequiredArgsConstructor
+public class BoardLayout implements BoardAdapter {
+
+    private final Twilight plugin;
+
+    @Override
+    public String getTitle(Player player) {
+        return CC.MAIN + CC.BOLD + "Twilight";
+    }
+
+    @Override
+    public List<String> getLines(Player player) {
+        final List<String> lines = new ArrayList<>();
+
+        lines.add(CC.SMALL_BAR);
+        lines.add("&fOnline: " + CC.SECOND + plugin.getOnline().size());
+        lines.add(" ");
+        lines.add("&7&olucanius.me");
+        lines.add(CC.SMALL_BAR);
+
+        return lines;
+    }
+}
