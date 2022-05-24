17cd24fd1530e707afd167d74fa268812f41c759
Lucanius69 <lucaisdik7@gmail.com>
2022-05-24 21:58:02 +0200

    Working on ArenaGenerator, not done yet.

--- src/main/java/me/lucanius/twilight/service/arena/generator/ArenaGenerator.java
+++ src/main/java/me/lucanius/twilight/service/arena/generator/ArenaGenerator.java
@@ -1,11 +1,17 @@
 package me.lucanius.twilight.service.arena.generator;
 
-import com.boydti.fawe.object.schematic.Schematic;
+import com.boydti.fawe.FaweAPI;
 import com.boydti.fawe.util.TaskManager;
+import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
+import com.sk89q.worldedit.function.operation.ForwardExtentCopy;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.regions.CuboidRegion;
-import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.world.registry.LegacyWorldData;
 import me.lucanius.twilight.Twilight;
 import me.lucanius.twilight.service.arena.Arena;
 import me.lucanius.twilight.tools.CC;
@@ -16,7 +22,6 @@ import org.bukkit.Material;
 import org.bukkit.World;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  * @author Lucanius
@@ -28,21 +33,29 @@ public class ArenaGenerator {
 
     private final Arena arena;
     private final World world;
-    private final Schematic schematic;
+
+    private final CuboidRegion region;
+    private final BlockArrayClipboard clipboard;
 
     private int multiplier = 10000;
 
-    @SuppressWarnings("deprecation")
     public ArenaGenerator(Arena arena) {
         this.arena = arena;
         this.world = arena.getMin().getBukkitWorld();
-        this.schematic = new Schematic(
-                new CuboidRegion(
-                        new BukkitWorld(this.world),
-                        new Vector(arena.getMin().getX(), arena.getMin().getY(), arena.getMin().getZ()),
-                        new Vector(arena.getMax().getX(), arena.getMax().getY(), arena.getMax().getZ())
-                )
+
+        this.region = new CuboidRegion(
+                FaweAPI.getWorld(arena.getMin().getWorld()),
+                new Vector(arena.getMin().getX(), arena.getMin().getY(), arena.getMin().getZ()),
+                new Vector(arena.getMax().getX(), arena.getMax().getY(), arena.getMax().getZ())
         );
+        this.clipboard = new BlockArrayClipboard(region);
+        try {
+            Operations.complete(new ForwardExtentCopy(
+                    FaweAPI.getWorld(arena.getMin().getWorld()), region, clipboard, region.getMinimumPoint()
+            ));
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void generate() {
@@ -58,21 +71,16 @@ public class ArenaGenerator {
             arena.setMiddle(new SerializableLocation(world.getName(), (arena.getA().getX() + arena.getB().getX()) / 2, arena.getBuildHeight(), (arena.getA().getZ() + arena.getB().getZ()) / 2));
         }
 
-        double x = arena.getMiddle().getX();
-        double z = arena.getMiddle().getZ();
+        double x = (arena.getMiddle().getX() + multiplier);
+        double y = (arena.getA().getY() + arena.getB().getY()) / 2;
+        double z = (arena.getMiddle().getZ() + multiplier);
 
-        Region region = Objects.requireNonNull(schematic.getClipboard()).getRegion();
-        if (region == null) {
-            Tools.log("&cFailed to generate arena: region is null");
-            return;
-        }
-
-        int minX = (int) (x - region.getWidth() + multiplier);
-        int maxX = (int) (x + region.getWidth() + multiplier);
-        int minZ = (int) (z - region.getLength() + multiplier);
-        int maxZ = (int) (z + region.getLength() + multiplier);
-        int minY = (int) arena.getMin().getY();
-        int maxY = (int) arena.getMax().getY();
+        int minX = (int) (x - region.getWidth() - 200);
+        int maxX = (int) (x + region.getWidth() + 200);
+        int minZ = (int) (z - region.getLength() - 200);
+        int maxZ = (int) (z + region.getLength() + 200);
+        int minY = (int) (y - region.getHeight());
+        int maxY = (int) (y + region.getHeight());
 
         boolean canPaste = true;
         for (int x1 = minX; x1 <= maxX; x1++) {
@@ -93,13 +101,19 @@ public class ArenaGenerator {
             return;
         }
 
-        double usedX = x + multiplier;
-        double usedZ = z + multiplier;
-        double usedY = arena.getMiddle().getY();
-
         Tools.log("&aPasting arena...");
         TaskManager.IMP.async(() -> {
-            schematic.paste(region.getWorld(), new Vector(usedX, usedY, usedZ));
+            Tools.log("Pasting at: " + x + " " + y + " " + z);
+            EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(FaweAPI.getWorld(arena.getMin().getWorld()), -1);
+            try {
+                Operation operation = new ClipboardHolder(clipboard, LegacyWorldData.getInstance())
+                        .createPaste(editSession, LegacyWorldData.getInstance())
+                        .to(new Vector(x, y, z))
+                        .build();
+                Operations.complete(operation);
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
 
             try {
                 Thread.sleep(1000L);
@@ -124,7 +138,7 @@ public class ArenaGenerator {
             Clickable clickable = new Clickable(
                     CC.MAIN + CC.BOLD + "(Twilight) " + CC.SECOND + "Generated an arena! " + CC.GRAY + "(Click to teleport)",
                     CC.SECOND + CC.ITALIC + "Click to teleport",
-                    "/tp " + newArena.getA().getX() + " " + newArena.getA().getY() + " " + newArena.getA().getZ()
+                    "/minecraft:tp " + newArena.getA().getX() + " " + newArena.getA().getY() + " " + newArena.getA().getZ()
             );
             plugin.getOnline().stream().filter(online -> online.hasPermission("twilight.admin")).forEach(clickable::send);
         });
