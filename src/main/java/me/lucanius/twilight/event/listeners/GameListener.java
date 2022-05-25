2bf3fcfd166fb5f625835afb62bb6cb58cff6567
Lucanius69 <lucaisdik7@gmail.com>
2022-05-25 15:31:17 +0200

    Implement Events.subscribe method on `GameListener`

--- src/main/java/me/lucanius/twilight/event/listeners/GameListener.java
+++ src/main/java/me/lucanius/twilight/event/listeners/GameListener.java
@@ -23,57 +23,47 @@ import java.util.stream.Collectors;
  * @author Lucanius
  * @since May 25, 2022
  */
-public class GameListener implements EventListener {
+public class GameListener {
 
     private final Twilight plugin = Twilight.getInstance();
 
     public GameListener() {
         Events.subscribe(GameStartEvent.class, event -> {
+            Game game = event.getGame();
+            // TODO: Check if game is buildable, get arena copy and set it to game, cancel event when arena copy is null
 
+            Collection<TeamMember> members = game.getMembers();
+            members.forEach(member -> {
+                Profile profile = member.getProfile();
+                profile.setState(ProfileState.PLAYING);
+                GameProfile gameProfile = profile.getGameProfile();
+
+                gameProfile.reset();
+                gameProfile.setGameId(game.getUniqueId());
+
+                GameTeam team = member.getTeam();
+                gameProfile.setTeam(team);
+
+                Player player = member.getPlayer();
+                Tools.clearPlayer(player);
+
+                if (team.getSpawn() == null) {
+                    team.setSpawn(team.getColor() == ChatColor.BLUE ? game.getArena().getA().getBukkitLocation() : game.getArena().getB().getBukkitLocation());
+                }
+                player.teleport(team.getSpawn());
+            });
+
+            Collection<Player> players = members.stream().map(TeamMember::getPlayer).collect(Collectors.toList());
+            Scheduler.run(() -> {
+                players.forEach(player -> plugin.getOnline().forEach(online -> {
+                    online.hidePlayer(player);
+                    player.hidePlayer(online);
+                }));
+
+                players.forEach(player -> players.forEach(player::showPlayer));
+            });
+
+            // start new GameTask
         });
-    }
-
-    @Override
-    public void onEvent(TwilightEvent twilightEvent) {
-        if (!(twilightEvent instanceof GameStartEvent)) {
-            return;
-        }
-
-        GameStartEvent event = (GameStartEvent) twilightEvent;
-        Game game = event.getGame();
-        // TODO: Check if game is buildable, get arena copy and set it to game, cancel event when arena copy is null
-
-        Collection<TeamMember> members = game.getMembers();
-        members.forEach(member -> {
-            Profile profile = member.getProfile();
-            profile.setState(ProfileState.PLAYING);
-            GameProfile gameProfile = profile.getGameProfile();
-
-            gameProfile.reset();
-            gameProfile.setGameId(game.getUniqueId());
-
-            GameTeam team = member.getTeam();
-            gameProfile.setTeam(team);
-
-            Player player = member.getPlayer();
-            Tools.clearPlayer(player);
-
-            if (team.getSpawn() == null) {
-                team.setSpawn(team.getColor() == ChatColor.BLUE ? game.getArena().getA().getBukkitLocation() : game.getArena().getB().getBukkitLocation());
-            }
-            player.teleport(team.getSpawn());
-        });
-
-        Collection<Player> players = members.stream().map(TeamMember::getPlayer).collect(Collectors.toList());
-        Scheduler.run(() -> {
-            players.forEach(player -> plugin.getOnline().forEach(online -> {
-                online.hidePlayer(player);
-                player.hidePlayer(online);
-            }));
-
-            players.forEach(player -> players.forEach(player::showPlayer));
-        });
-
-        // start new GameTask
     }
 }
