4145651c5e145f9b0856c8dcf4381f10495cfd7a
Lucanius69 <lucaisdik7@gmail.com>
2022-05-25 13:11:40 +0200

    Start on `GameService`

--- src/main/java/me/lucanius/twilight/service/game/GameService.java
+++ src/main/java/me/lucanius/twilight/service/game/GameService.java
@@ -1,8 +1,22 @@
 package me.lucanius.twilight.service.game;
 
+import me.lucanius.twilight.Twilight;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * @author Lucanius
  * @since May 25, 2022
  */
 public class GameService {
+
+    private final Twilight plugin;
+    private final Map<UUID, Game> games;
+
+    public GameService(Twilight plugin) {
+        this.plugin = plugin;
+        this.games = new HashMap<>();
+    }
 }
