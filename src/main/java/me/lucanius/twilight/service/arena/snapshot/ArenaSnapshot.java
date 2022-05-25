416f4d3c644f54bee87882c0823b98594186992a
Lucanius69 <lucaisdik7@gmail.com>
2022-05-25 14:12:14 +0200

    Replace `ArenaSnapshot` class

---
+++ src/main/java/me/lucanius/twilight/service/arena/snapshot/ArenaSnapshot.java
@@ -0,0 +1,47 @@
+package me.lucanius.twilight.service.arena.snapshot;
+
+import me.lucanius.twilight.service.arena.Arena;
+import me.lucanius.twilight.tools.ChunkVector;
+import org.bukkit.ChunkSnapshot;
+import org.bukkit.material.MaterialData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Lucanius
+ * @since May 25, 2022
+ *
+ * I don't know if this will even work
+ */
+public class ArenaSnapshot {
+
+    private final Map<ChunkVector, ChunkSnapshot> snapshots = new HashMap<>();
+
+    public void save(Arena arena) {
+        arena.getCuboid().getChunks().forEach(chunk ->
+                snapshots.put(ChunkVector.of(chunk), chunk.getChunkSnapshot())
+        );
+    }
+
+    public void restore(Arena arena) {
+        arena.getCuboid().getChunks().forEach(chunk -> {
+            ChunkVector chunkVector = ChunkVector.of(chunk);
+            ChunkSnapshot snapshot = snapshots.get(chunkVector);
+            if (snapshot != null) {
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        for (int y = 0; y < 256; y++) {
+                            chunk.getBlock(x, y, z).getState().setData(
+                                    new MaterialData(
+                                            snapshot.getBlockTypeId(x, y, z),
+                                            (byte) snapshot.getBlockData(x, y, z)
+                                    )
+                            );
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
