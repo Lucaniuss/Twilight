944f7e9b6f4aee5c2f56cd309a5dac8b4bab1eb8
Lucanius69 <lucaisdik7@gmail.com>
2022-05-25 17:11:38 +0200

    Remove static `Twilight` from `TwilightExecutor<E>` and optimize imports

--- src/main/java/me/lucanius/twilight/event/TwilightExecutor.java
+++ src/main/java/me/lucanius/twilight/event/TwilightExecutor.java
@@ -1,17 +1,11 @@
 package me.lucanius.twilight.event;
 
-import me.lucanius.twilight.Twilight;
-import me.lucanius.twilight.event.EventListener;
-import me.lucanius.twilight.event.TwilightEvent;
-
 /**
  * @author Lucanius
  * @since May 25, 2022
  */
 @FunctionalInterface
 public interface TwilightExecutor<E> extends EventListener {
-
-    Twilight plugin = Twilight.getInstance();
 
     void callEvent(E event);
 
