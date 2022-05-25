0e5b5c4d4e8aa61bf0ef8cbf1c8ccf2925efdd50
Lucanius69 <lucaisdik7@gmail.com>
2022-05-25 14:37:35 +0200

    Add messages on `QueueCallback`

--- src/main/java/me/lucanius/twilight/service/queue/callback/QueueCallback.java
+++ src/main/java/me/lucanius/twilight/service/queue/callback/QueueCallback.java
@@ -1,12 +1,20 @@
 package me.lucanius.twilight.service.queue.callback;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import me.lucanius.twilight.tools.CC;
+
 /**
  * @author Lucanius
  * @since May 22, 2022
  */
+@Getter @AllArgsConstructor
 public enum QueueCallback {
 
-    ALLOWED,
-    DENIED
+    NONE(""),
+    ALLOWED(CC.GREEN + "Starting game..."),
+    DENIED(CC.RED + "An error occurred while attempting to start a game.");
+
+    private final String message;
 
 }
