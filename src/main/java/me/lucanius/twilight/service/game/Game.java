8a94927720c72f86e579b618a186a29b9ca27326
Lucanius69 <lucaisdik7@gmail.com>
2022-05-25 13:11:05 +0200

    Add some methods to `Game`

--- src/main/java/me/lucanius/twilight/service/game/Game.java
+++ src/main/java/me/lucanius/twilight/service/game/Game.java
@@ -1,43 +1,88 @@
 package me.lucanius.twilight.service.game;
 
+import lombok.Getter;
+import lombok.Setter;
 import me.lucanius.twilight.Twilight;
 import me.lucanius.twilight.service.arena.Arena;
+import me.lucanius.twilight.service.game.context.GameContext;
+import me.lucanius.twilight.service.game.context.GameState;
+import me.lucanius.twilight.service.game.team.GameTeam;
+import me.lucanius.twilight.service.game.team.member.TeamMember;
 import me.lucanius.twilight.service.loadout.Loadout;
 import me.lucanius.twilight.service.queue.abstr.AbstractQueue;
+import me.lucanius.twilight.tools.CC;
+import me.lucanius.twilight.tools.date.DateTools;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Lucanius
  * @since May 25, 2022
  */
+@Getter @Setter
 public class Game {
 
     private final static Twilight plugin = Twilight.getInstance();
 
     private final Set<Location> blocks;
     private final UUID uniqueId;
-    private final GameType type;
+    private final GameContext context;
     private final Loadout loadout;
     private final Arena arena;
     private final AbstractQueue<?> queue;
     private final List<GameTeam> teams;
 
     private GameState state;
+    private long timeStamp;
 
-    public Game(GameType type, Loadout loadout, Arena arena, AbstractQueue<?> queue, List<GameTeam> teams) {
+    public Game(GameContext context, Loadout loadout, Arena arena, AbstractQueue<?> queue, List<GameTeam> teams) {
         this.blocks = new HashSet<>();
         this.uniqueId = UUID.randomUUID();
-        this.type = type;
+        this.context = context;
         this.loadout = loadout;
         this.arena = arena;
         this.queue = queue;
         this.teams = teams;
 
         this.state = GameState.STARTING;
+        this.timeStamp = System.currentTimeMillis();
+    }
+
+    public Collection<Player> getEveryone() {
+        return teams.stream().flatMap(team -> team.getMembers().stream()).map(TeamMember::getPlayer).collect(Collectors.toList());
+    }
+
+    public Collection<Player> getAlive() {
+        return teams.stream().flatMap(team -> team.getMembers().stream()).filter(TeamMember::isAlive).map(TeamMember::getPlayer).collect(Collectors.toList());
+    }
+
+    public GameTeam getTeam(UUID uniqueId) {
+        return teams.stream().filter(team -> team.getSpecific(uniqueId) != null).findFirst().orElse(null);
+    }
+
+    public GameTeam getOpposingTeam(UUID uniqueId) {
+        return teams.stream().filter(team -> team.getSpecific(uniqueId) == null).findFirst().orElse(null);
+    }
+
+    public void sendMessage(String message) {
+        getEveryone().forEach(player -> player.sendMessage(CC.translate(message)));
+    }
+
+    public void sendSound(Sound sound) {
+        getEveryone().forEach(player -> player.playSound(player.getLocation(), sound, 1, 1));
+    }
+
+    public void clearBlocks() {
+        blocks.forEach(location -> location.getBlock().setType(Material.AIR));
+        blocks.clear();
+    }
+
+    public String getTime() {
+        return DateTools.formatIntToMMSS((int) ((System.currentTimeMillis() - timeStamp) / 1000L));
     }
 }
