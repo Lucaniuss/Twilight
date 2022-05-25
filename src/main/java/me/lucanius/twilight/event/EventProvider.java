8ce69c84282eb6b785f52d55a770457874cfd890
Clouke <76826179+Clouke@users.noreply.github.com>
2022-05-25 14:44:52 +0200

    create `EventProvider`

---
+++ src/main/java/me/lucanius/twilight/event/EventProvider.java
@@ -0,0 +1,18 @@
+package me.lucanius.twilight.event;
+
+/**
+ * @author Clouke
+ * @since 25.05.2022 14:23
+ * © Twilight - All Rights Reserved
+ */
+public interface EventProvider {
+
+    void subscribe(EventListener abstractEvent);
+
+    void unsubscribe(EventListener abstractEvent);
+
+    void publish(TwilightEvent abstractEvent);
+
+    <T extends TwilightEvent> void publish(Class<T> event);
+
+}
