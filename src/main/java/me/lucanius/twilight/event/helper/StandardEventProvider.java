c0feeac58ac8ef3d45ddcf85973d20a5e03f19d6
Lucanius69 <lucaisdik7@gmail.com>
2022-05-27 15:11:48 +0200

    Cleaned `StandardEventProvider`

--- src/main/java/me/lucanius/twilight/event/helper/StandardEventProvider.java
+++ src/main/java/me/lucanius/twilight/event/helper/StandardEventProvider.java
@@ -1,6 +1,5 @@
 package me.lucanius.twilight.event.helper;
 
-import com.google.common.base.Preconditions;
 import me.lucanius.twilight.event.AbstractEvent;
 import me.lucanius.twilight.event.EventListener;
 import me.lucanius.twilight.event.EventProvider;
@@ -23,54 +22,44 @@ public class StandardEventProvider implements EventProvider {
 
     @Override
     public void subscribe(Class<? extends AbstractEvent> event, EventListener listener) {
-        Preconditions.checkState(!this.subscribers.containsKey(listener), "Event already subscribed");
-        this.subscribers.put(listener, event);
-    }
-
-    @Override
-    public void unsubscribe(EventListener listener) {
-        Preconditions.checkState(this.subscribers.containsKey(listener), "Event not subscribed");
-        this.subscribers.remove(listener);
-    }
-
-    @Override
-    public void unsubscribe(Class<? extends AbstractEvent> event) {
-        for (Map.Entry<EventListener, Class<? extends AbstractEvent>> entry : this.subscribers.entrySet()) {
-            if (entry.getValue().equals(event)) {
-                this.subscribers.remove(entry.getKey());
-            }
+        if (!subscribers.containsKey(listener)) {
+            subscribers.put(listener, event);
         }
     }
 
     @Override
+    public void unsubscribe(EventListener listener) {
+        subscribers.remove(listener);
+    }
+
+    @Override
+    public void unsubscribe(Class<? extends AbstractEvent> event) {
+        subscribers.keySet().removeIf(listener -> subscribers.get(listener).equals(event));
+    }
+
+    @Override
     public boolean subbed(EventListener listener) {
-        return this.subscribers.containsKey(listener);
+        return subscribers.containsKey(listener);
     }
 
     @Override
     public boolean subbed(Class<? extends AbstractEvent> event) {
-        return this.subscribers.containsValue(event);
+        return subscribers.containsValue(event);
     }
 
     @Override
     public void publish(TwilightEvent event) {
-        this.thread.execute(() -> {
-            for (Map.Entry<EventListener, Class<? extends AbstractEvent>> entry : this.subscribers.entrySet()) {
-                if (entry.getValue().isInstance(event)) {
-                    entry.getKey().onEvent(event);
-                }
-            }
-        });
+        this.thread.execute(() -> subscribers.entrySet().stream()
+                .filter(entry -> entry.getValue().isInstance(event))
+                .forEach(entry -> entry.getKey().onEvent(event))
+        );
     }
 
     @Override
     public <T extends TwilightEvent> void publish(Class<T> event) {
-        this.thread.execute(() -> {
-            for (Map.Entry<EventListener, Class<? extends AbstractEvent>> entry : this.subscribers.entrySet()) {
-                if (entry.getValue().equals(event)) {
-                    entry.getKey().onEvent(event);
-                }
-            }
-        });
+        this.thread.execute(() -> subscribers.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(event))
+                .forEach(entry -> entry.getKey().onEvent(event))
+        );
     }
 }
