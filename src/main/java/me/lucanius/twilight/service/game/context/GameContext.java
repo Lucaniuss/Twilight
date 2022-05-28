eabb68d767fe58f5837ea6b634cbf3315141a518
Lucanius69 <lucaisdik7@gmail.com>
2022-05-28 19:51:34 +0200

    Add some features to `GameContext`

--- src/main/java/me/lucanius/twilight/service/game/context/GameContext.java
+++ src/main/java/me/lucanius/twilight/service/game/context/GameContext.java
@@ -1,13 +1,23 @@
 package me.lucanius.twilight.service.game.context;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.Material;
+
 /**
  * @author Lucanius
  * @since May 25, 2022
  */
+@Getter @AllArgsConstructor
 public enum GameContext {
 
-    NORMAL,
-    FFA,
-    SPLIT
+    NORMAL("Normal", false, 0, Material.IRON_SWORD),
+    FFA("Ffa", true, 2, Material.BLAZE_POWDER),
+    SPLIT("Split", true, 6, Material.QUARTZ);
+
+    private final String name;
+    private final boolean party;
+    private final int slot;
+    private final Material material;
 
 }
