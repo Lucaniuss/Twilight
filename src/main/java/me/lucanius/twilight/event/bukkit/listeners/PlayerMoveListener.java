d0c2ba6b9522a114251583838457fc5849df5bc7
Lucanius69 <lucaisdik7@gmail.com>
2022-06-01 20:55:23 +0200

    Optimize `PlayerMoveListener`

--- src/main/java/me/lucanius/twilight/event/bukkit/listeners/PlayerMoveListener.java
+++ src/main/java/me/lucanius/twilight/event/bukkit/listeners/PlayerMoveListener.java
@@ -1,5 +1,6 @@
 package me.lucanius.twilight.event.bukkit.listeners;
 
+import me.lucanius.twilight.Twilight;
 import me.lucanius.twilight.event.bukkit.Events;
 import me.lucanius.twilight.event.events.AsyncMovementEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -10,10 +11,18 @@ import org.bukkit.event.player.PlayerMoveEvent;
  */
 public class PlayerMoveListener {
 
+    private final Twilight plugin = Twilight.getInstance();
+
     /**
      * Running our custom AsyncMovementEvent through the Bukkit PlayerMoveEvent
      */
     public PlayerMoveListener() {
-        Events.subscribe(PlayerMoveEvent.class, AsyncMovementEvent::new);
+        Events.subscribe(PlayerMoveEvent.class, event -> {
+            if (!plugin.getEvents().subbed(AsyncMovementEvent.class)) {
+                return;
+            }
+
+            new AsyncMovementEvent(event);
+        });
     }
 }
